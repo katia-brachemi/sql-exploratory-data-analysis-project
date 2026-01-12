50d562513b674e02616beb4493d9771fdf7c5833
Katia Ourida Brachemi <katiabrachemi42@gmail.com>
2026-01-12 14:32:37 +0100

    Update 08_customer_behavior_analysis.sql

--- script/08_customer_behavior_analysis.sql
+++ script/08_customer_behavior_analysis.sql
@@ -1,10 +1,13 @@
 /*
---================================================================
+===========================================================================================
 Customer Behavior Analysis
---================================================================
+===========================================================================================
 Purpose:
     - Summarize customer activity using Recency, Frequency, and Monetary (RFM).
     - Build RFM metrics (Recency, Frequency, Monetary) for each customer.
+    - Analyze customer spending behavior by gender or age group. 
+    - Age is calculated precisely from birth_date, adjusting for birthdays not yet reached.
+    - Unrealistic ages are excluded to ensure data quality.
 
   Terms Used:
    - Recency: Most recent purchase date 
@@ -12,10 +15,12 @@ Purpose:
    - Monetary: Total spend
 
 SQL Functions Used:
-   - Aggregate Functions: SUM(), COUNT()
+   - Aggregate Functions: SUM(), COUNT(), AVG()
    - Window Functions
    - GROUP BY, ORDER BY
-===============================================================================
+   - Date Functions: DATEDIFF(), DATEADD()
+   - Conditional Logic: CASE
+==============================================================================================
 */
 
 WITH rfm_base AS (
@@ -44,4 +49,34 @@ rfm_scored AS (
 SELECT *
 FROM rfm_scored
 ORDER BY monetary DESC;
+
+
+-- Average spend by gender
+SELECT gender, 
+AVG(sales_amount) AS avg_spend
+FROM gold.fact_sales s
+JOIN gold.dim_customers c 
+ON s.customer_key = c.customer_key
+GROUP BY gender;
+
+
+-- Customer behavior by age
+WITH customer_age AS ( 
+SELECT 
+s.sales_amount,
+DATEDIFF(YEAR, c.birth_date, GETDATE()) 
+- CASE WHEN DATEADD(YEAR, DATEDIFF(YEAR, c.birth_date, GETDATE()), c.birth_date)
+> GETDATE() 
+THEN 1 
+ELSE 0 
+END AS age 
+FROM gold.fact_sales s 
+JOIN gold.dim_customers c 
+ON s.customer_key = c.customer_key ) 
+SELECT age, 
+AVG(sales_amount) AS avg_spend 
+FROM customer_age 
+WHERE age BETWEEN 18 AND 100 
+GROUP BY age;
+
 
